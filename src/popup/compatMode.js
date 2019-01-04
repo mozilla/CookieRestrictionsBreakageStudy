b53c8ee3a2e5af36a5e5bda30dc3087ebbc0c764
Erica Wright <ericaewright@gmail.com>
2019-01-04 13:28:05 -0500

    Style the popup.

--- src/popup/compatMode.js
+++ src/popup/compatMode.js
@@ -2,14 +2,12 @@ function getCurrentWindowActiveTab() {
   return browser.tabs.query({currentWindow: true, active: true});
 }
 
-const checkbox = document.getElementById("unbreakCheck");
-checkbox.addEventListener("click", (e) => {
-  if (checkbox.checked) {
-    getCurrentWindowActiveTab().then((tabList) => {
-      const activeTabID = tabList[0].id;
-      browser.runtime.sendMessage({tabId: activeTabID});
-    });
-  } else {
-    console.log("unchecking");
-  }
+const button = document.getElementById("FixThisSite");
+button.addEventListener("click", (e) => {
+  getCurrentWindowActiveTab().then((tabList) => {
+    const activeTabID = tabList[0].id;
+    // Send message to feature.js to turn on compat mode.
+    browser.runtime.sendMessage({tabId: activeTabID});
+  });
+  window.close();
 });
