83d1ce6d598a4849d6d3d6bff06cf27fbfc14149
Erica Wright <ericaewright@gmail.com>
2019-01-04 13:58:49 -0500

    Style for private browsing mode.

--- src/popup/compatMode.js
+++ src/popup/compatMode.js
@@ -1,8 +1,15 @@
+browser.windows.getCurrent().then((windowInfo) => {
+  if (windowInfo.incognito) {
+    document.getElementById("basicBrowsing").hidden = true;
+    document.getElementById("privateBrowsing").hidden = false;
+  }
+});
+
 function getCurrentWindowActiveTab() {
   return browser.tabs.query({currentWindow: true, active: true});
 }
 
-const button = document.getElementById("FixThisSite");
+const button = document.getElementById("fixThisSite");
 button.addEventListener("click", (e) => {
   getCurrentWindowActiveTab().then((tabList) => {
     const activeTabID = tabList[0].id;
